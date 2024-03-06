d6f043224d1175938cdd8dc0f9afe47d4c2f4d69
Joyce <joycesantos8900@gmail.com>
2024-03-06 12:37:44 -0300

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from 'react';
-import './App.css'
+import './App.css';
 import { FaSearchLocation } from "react-icons/fa";
-
+import api from './services/api';
 
 function App() {
-  const [input,setInput] = useState()
-  const handleSearch = ()=>alert("Teste" + input)
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState("");
+
+  async function handleSearch() {
+    try {
+      if (input === "") {
+        alert("Preencha algum CEP!");
+        return;
+      }
+
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput("");
+
+     
+    } catch (error) {
+      alert("Erro ao buscar");
+      
+    }
+  }
 
   return (
     <div className='container'>
@@ -13,22 +31,27 @@ function App() {
       <div className='containerInput' >
         <input type="text" placeholder="Digite seu CEP..." 
           value={input}
-          onChange={(e)=> setInput(e.target.value)}
+          onChange={(e) => setInput(e.target.value)}
         />
         <button className='buttonSearch' onClick={handleSearch}>
           <FaSearchLocation size={25} color='#ffff'/>
         </button>
       </div>
-      <main className='main'>
-        <h2>CEP: 59330000</h2>
+      {Object.keys(cep).length >0 && (
+        <main className='main'>
+                <h2>CEP:{cep.cep}</h2>
 
-        <span>Rua: Manoel Pereira da Cruz</span>
-        <span>Bairro: Centro</span>
-        <span>Cidade: Jucurutu</span>
-        <span>Estdado: Rio Grande do Norte</span>
-      </main>
+                <span>Rua: {cep.logradouro}</span>
+                <span>Bairro: {cep.bairro}</span>
+                <span>Cidade: {cep.localidade}</span>
+                <span>Estado: {cep.uf}</span>
+                <span>DDD: {cep.ddd}</span>
+              </main>
+      )
+      }
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
